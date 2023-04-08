153fd5c89405bcebc472e9d61261db967aa4662e
Pinbib <pancenkoe353@gmail.com>
2023-04-08 13:39:20 +0100

    module 'rompt' was added and errors removed

---
+++ module/prompt.js
@@ -0,0 +1,61 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+module.exports = (body, mainpath, door, variable, comment, declare, tool) => {
+    const { Variable, Formation } = require(tool);
+    let data = body.split(" ");
+    switch (data[1]) {
+        case "readline":
+            if (true) {
+                Variable.set(variable, data[2], prompt(
+                    Formation.var(
+                        variable, Formation.com(
+                            comment, Formation.var(
+                                variable, data.slice(3).join(" ")
+                            )
+                        )
+                    )
+                )
+                );
+            }
+            break;
+        case "multiline":
+            if (true) {
+                var end = data[3];
+                var cirkle = [];
+                var a = true;
+                console.log(Formation.var(
+                    variable, Formation.com(
+                        comment, Formation.var(
+                            variable, data.slice(4).join(" ")
+                        )
+                    )
+                ));
+                while (a) {
+                    var value = prompt("");
+                    if (value == end) {
+                        Variable.set(variable, data[2], cirkle.join("\n"));
+                        a = false;
+                    } else {
+                        cirkle.push(value);
+                    };
+                }
+            }
+            break;
+        case "key":
+            if (true) {
+                const prompt = require("prompt-sync")({ sigint: true, echo: false });
+                Variable.set(variable, data[2], prompt(
+                    Formation.var(
+                        variable, Formation.com(
+                            comment, Formation.var(
+                                variable, data.slice(3).join(" ")
+                            )
+                        )
+                    )
+                )
+                    .split("")
+                );
+            }
+            break;
+    }
+};
--- tools.js
+++ tools.js
@@ -14,4 +14,38 @@ class Variable {
     }
 }
 
-module.exports = { Variable };
+class Formation {
+    static var(variable, text) {
+        let newtext = text.split(" ");
+        var end = [];
+        for (var i = 0; i < newtext.length; i++) {
+            if (/^\@/gm.test(newtext[i])) {
+                end.push(variable[newtext[i].replace(/^\@/gm, "")]);
+            } else if (newtext[i] == "") {
+
+            } else {
+                end.push(newtext[i]);
+            };
+        };
+        return end.join(" ");
+    };
+    static com(comment, text) {
+        let newtext = text.split(" ");
+        var end = [];
+        for (var i = 0; i < newtext.length; i++) {
+            if (/^\!/gm.test(newtext[i])) {
+                if (/^[0-9]/gm.test(newtext[i + 1])) {
+                    end.push(comment[newtext[i + 1]]);
+                    newtext[i + 1] = "";
+                } else {
+                    console.error(`After the sign "!" a number must go as this is a reference to the global comment which is an array.`);
+                }
+            } else if (newtext[i] == "") { } else {
+                end.push(newtext[i]);
+            };
+        };
+        return end.join(" ");
+    };
+};
+
+module.exports = { Variable, Formation };
